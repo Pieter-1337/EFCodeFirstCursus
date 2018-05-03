a92f90c90fc785d5c6269f2ce9987530dce13045
pieter bracke <pieterbracke@msn.com>
2018-05-03 09:15:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
